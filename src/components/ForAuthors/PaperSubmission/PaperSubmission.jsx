9327cbfbeac3ae639fcb3de9fd35132df923f38c
AlienMinus <dasmanasranjan2005@gmail.com>
2026-05-29 16:36:42 +0530

    minor fix

--- src/components/ForAuthors/PaperSubmission/PaperSubmission.jsx
+++ src/components/ForAuthors/PaperSubmission/PaperSubmission.jsx
@@ -79,7 +79,7 @@ const PaperSubmission = () => {
 
       <div className="ps-note">
         <h2>{note.title}</h2>
-        <p>{note.description}</p>
+        <p>The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.</p>
       </div>
 
       <div className="ps-footer">
